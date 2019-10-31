2e02a7530422c553043b725c94d3d05eb3bb7d14
Evan Schwarz <evanschwarz90@gmail.com>
2019-10-30 19:33:05 -0600

    server skeleton with directories for MVC

---
+++ routes.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+
+routes.get('/list', () => {});
+
+routes.get('/:product_id', () => {});
+
+routes.get('/:product_id/styles', () => {});
+
+routes.get('/:product_id/related', () => {});
